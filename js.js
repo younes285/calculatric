774c8f8b6d6a06d9a02944dd12e7e28201073bf9
younes285 <141585964+younes285@users.noreply.github.com>
2023-08-12 19:19:11 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+const number=document.querySelector("numbers")
+
+const addButton = document.querySelector('[value="addition"]');
+const subtractButton = document.querySelector('[value="subtraction"]');
+const multiplyButton = document.querySelector('[value="multiplication"]');
+const divideButton = document.querySelector('[value="division"]');
+
+addButton.addEventListener('click')
+subtractButton.addEventListener('click')
+multiplyButton.addEventListener('click')
+divideButton.addEventListener('click')
+
+function calculate () {
+
+}
+
+
